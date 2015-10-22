fdd495d029142706d51e601e50c19cacbfc21e8b
mahilion <mahilion01@gmail.com>
2015-10-22 16:38:14 +0530

    ex3_sum

--- ex3_sum.c
+++ ex3_sum.c
@@ -1,1 +1,15 @@
+#include<stdio.h>
+int main(){
+int a=0,b=0,sum=0;
+printf("enter first number to add:");
+scanf("%d",&a);
+printf("\nenter second number to add:");
+scanf("%d",&b);
+sum=a+b;
+printf("\nthe sum of %d and %d is %d",a,b,sum);
+return 0;
+
+
+
+}
 
